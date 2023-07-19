4e927f87d7f9c3f2956bd0a607b9e16ae649b349
japhethnyaranga <nyaranga4@gmail.com>
2023-07-19 22:42:22 +0300

    The application is able to fetch the api

--- src/Pages/FeedPage.vue
+++ src/Pages/FeedPage.vue
@@ -1,56 +1,64 @@
 <template>
-    <topNavbar></topNavbar>
+    <topNavbar @search-query-changed="performSearch"></topNavbar>
 
     <div class="container">
-        
-        <div class="row mt-5">
-            <div class="col-md-4 col-sm-12">
+
+        <div class="row mt-5" v-if="posts.length">
+            <div class="col-md-4 col-sm-12" style="padding-top: 80px;" v-for="post in posts" :key="post.id">
                 <div class="card shadow-lg p-3 mb-5 bg-white rounded border-0">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                            card's content.</p>
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>
+                        <h5 class="card-title">{{ post.title }}</h5>
+                        
+                        <p class="card-text">{{ post.body }}</p>
+                        
                     </div>
                 </div>
             </div>
 
-            <div class="col-md-4 col-sm-12">
-                <div class="card shadow-lg p-3 mb-5 bg-white rounded border-0">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                            card's content.</p>
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="col-md-4 col-sm-12">
-                <div class="card shadow-lg p-3 mb-5 bg-white rounded border-0">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                            card's content.</p>
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
 </template>
 
 
 <script>
+import axios from 'axios';
 
-
+export default {
+    name: 'PostsList',
+    data() {
+        return {
+            posts: [],
+            searchQuery: '', // Add a searchQuery property to store the search query
+        };
+    },
+    mounted() {
+        this.fetchPosts();
+    },
+    methods: {
+        fetchPosts() {
+            // Fetch all posts
+            const apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+            axios
+                .get(apiUrl)
+                .then(response => {
+                    this.posts = response.data;
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+        },
+        performSearch(searchQuery) {
+            // Filter posts based on the search query
+            this.searchQuery = searchQuery;
+        },
+    },
+    computed: {
+        filteredPosts() {
+            // Filter posts based on the search query
+            return this.posts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()));
+        },
+    },
+};
 </script>
 
 <style scoped></style>
--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,140 +1,169 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #516BEB;">
-      <div class="container-fluid">
-        <a class="navbar-brand fw-bold" href="#">Elewa</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <!-- Your navigation links here -->
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Feed</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">My Feeds</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Login</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link ">Subscribe</a>
-            </li>
-          </ul>
-  
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">UserName</a>
-            </li>
-          
-          </ul>
-          <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-light" type="submit">Search</button>
-          </form>
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: #516BEB;">
+        <div class="container-fluid">
+            <a class="navbar-brand fw-bold" href="#">Elewa</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Your navigation links here -->
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Feed</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">My Feeds</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link ">Subscribe</a>
+                    </li>
+                </ul>
+
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">UserName</a>
+                    </li>
+
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"
+                        v-model="searchQuery" @input="performSearch">
+
+                </form>
+            </div>
         </div>
-      </div>
     </nav>
-  </template>
+</template>
   
 
   
 
 <script>
-
+export default {
+    data() {
+        return {
+            searchQuery: '', // Store the search query entered in the input field
+        };
+    },
+    methods: {
+        performSearch() {
+            // Emit an event to notify the parent component (PostsList) about the search query change
+            this.$emit('search-query-changed', this.searchQuery);
+        },
+    },
+};
 </script>
 
 
 <style scoped>
-    .navbar-brand {
-  font-size: 1.4em;
+.navbar-brand {
+    font-size: 1.4em;
 }
+
 .navbar-dark .navbar-nav a.nav-link {
-  color: #ffffff;
-  font-size: 1.1em;
-}
-.dropdown-menu {
-  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-  border: none;
-  border-radius: 0;
-  padding: 0.7em;
-}
-@media only screen and (min-width: 992px) {
-  .dropdown:hover .dropdown-menu {
-    display: flex;
-  }
-  .dropdown-menu.show {
-    display: flex;
-  }
-}
-.dropdown-menu ul {
-  list-style: none;
-  padding: 0;
-}
-.dropdown-menu li .dropdown-item {
-  color: gray;
-  font-size: 1em;
-  padding: 0.5em 1em;
-}
-.dropdown-menu li .dropdown-item:hover {
-  background-color: #f1f1f1;
-}
-.dropdown-menu li:first-child a {
-  font-weight: bold;
-  font-size: 1.1em;
-  text-transform: uppercase;
-  color: #516beb;
-}
-.dropdown-menu li:first-child a:hover {
-  background-color: #f1f1f1;
-}
-@media only screen and (max-width: 992px) {
-  .dropdown-menu.show {
-    flex-wrap: wrap;
-    max-height: 350px;
-    overflow-y: scroll;
-  }
-}
-@media only screen and (min-width: 992px) and (max-width: 1140px) {
-  .dropdown:hover .dropdown-menu {
-    width: 40vw;
-    flex-wrap: wrap;
-  }
+    color: #ffffff;
+    font-size: 1.1em;
 }
 
 .dropdown-menu {
-  border-radius: 0;
-  border: none;
-  padding: 0.5em;
-  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.23);
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+    border: none;
+    border-radius: 0;
+    padding: 0.7em;
 }
+
+@media only screen and (min-width: 992px) {
+    .dropdown:hover .dropdown-menu {
+        display: flex;
+    }
+
+    .dropdown-menu.show {
+        display: flex;
+    }
+}
+
 .dropdown-menu ul {
-  list-style: none;
-  padding: 0;
+    list-style: none;
+    padding: 0;
+}
+
+.dropdown-menu li .dropdown-item {
+    color: gray;
+    font-size: 1em;
+    padding: 0.5em 1em;
+}
+
+.dropdown-menu li .dropdown-item:hover {
+    background-color: #f1f1f1;
+}
+
+.dropdown-menu li:first-child a {
+    font-weight: bold;
+    font-size: 1.1em;
+    text-transform: uppercase;
+    color: #516beb;
+}
+
+.dropdown-menu li:first-child a:hover {
+    background-color: #f1f1f1;
+}
+
+@media only screen and (max-width: 992px) {
+    .dropdown-menu.show {
+        flex-wrap: wrap;
+        max-height: 350px;
+        overflow-y: scroll;
+    }
+}
+
+@media only screen and (min-width: 992px) and (max-width: 1140px) {
+    .dropdown:hover .dropdown-menu {
+        width: 40vw;
+        flex-wrap: wrap;
+    }
+}
+
+.dropdown-menu {
+    border-radius: 0;
+    border: none;
+    padding: 0.5em;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.23);
+}
+
+.dropdown-menu ul {
+    list-style: none;
+    padding: 0;
 }
 
 .dropdown-menu li a {
-  color: gray;
-  padding: 0.5em 1em;
-}
-.dropdown-menu li:first-child a {
-  font-weight: bold;
-  font-size: 1.1em;
-  color: #516beb;
-}
-@media screen and (min-width: 993px) {
-  .dropdown:hover .dropdown-menu {
-    display: flex;
-  }
-  .dropdown-menu.show {
-    display: flex;
-  }
-}
-@media screen and (max-width: 992px) {
-  .dropdown-menu.show {
-    max-height: 60vh;
-    overflow-y: scroll;
-  }
+    color: gray;
+    padding: 0.5em 1em;
 }
 
+.dropdown-menu li:first-child a {
+    font-weight: bold;
+    font-size: 1.1em;
+    color: #516beb;
+}
+
+@media screen and (min-width: 993px) {
+    .dropdown:hover .dropdown-menu {
+        display: flex;
+    }
+
+    .dropdown-menu.show {
+        display: flex;
+    }
+}
+
+@media screen and (max-width: 992px) {
+    .dropdown-menu.show {
+        max-height: 60vh;
+        overflow-y: scroll;
+    }
+}
 </style>
